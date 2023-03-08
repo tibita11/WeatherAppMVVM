7caa0c43a65ecf6abfffd482cf310918db473254
tibita11 <tibita0811@gmail.com>
2023-03-08 15:06:26 +0900

    feat: weatherVCのタブをCollectionViewで実装

---
+++ WeatherAppMVVM/View/LocationTabCollectionViewCell/LocationTabCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  LocationTabCollectionViewCell.swift
+//  WeatherAppMVVM
+//
+//  Created by 鈴木楓香 on 2023/03/08.
+//
+
+import UIKit
+
+class LocationTabCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var locationLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+    }
+
+}
--- WeatherAppMVVM/View/WeatherViewController.swift
+++ WeatherAppMVVM/View/WeatherViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import RxSwift
+import RxCocoa
 import RealmSwift
 import RxRealm
 
@@ -14,7 +15,9 @@ class WeatherViewController: UIViewController {
 
     @IBOutlet weak var collectionView: UICollectionView!
     
-    let disposeBag = DisposeBag()
+    private let disposeBag = DisposeBag()
+    /// CollectionViewに表示するデータを格納
+    private var locations: List<Location>?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,13 +29,19 @@ class WeatherViewController: UIViewController {
         // NavigationBar設定
         let registrationButton = UIBarButtonItem(title: "地点登録", style: .plain, target: self, action: #selector(navigateToRegistrationScreen))
         navigationItem.rightBarButtonItems = [registrationButton]
+        // CollectionView設定
+        collectionView.register(UINib(nibName: "LocationTabCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "locationCell")
+        collectionView.rx.setDataSource(self).disposed(by: disposeBag)
+        collectionView.rx.setDelegate(self).disposed(by: disposeBag)
         // Realm通知
         let realm = try! Realm()
-        let locationList = realm.objects(Location.self)
-
+        let locationList = realm.objects(LocationList.self)
+        // collectionViewとバインド
         Observable.collection(from: locationList)
-            .subscribe(onNext: { locations in
-                print("これはmain画面です:\(locationList)")
+            .subscribe(onNext: { [weak self] locationList in
+                // 取得したlistを格納
+                self!.locations = locationList.first?.list
+                self!.collectionView.reloadData()
             })
             .disposed(by: disposeBag)
     }
@@ -44,4 +53,37 @@ class WeatherViewController: UIViewController {
     }
 
 
+}
+
+
+// MARK: - UICollectionViewDataSource
+
+extension WeatherViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return locations?.count ?? 0
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "locationCell", for: indexPath) as! LocationTabCollectionViewCell
+        cell.locationLabel.text = locations?[indexPath.row].title ?? ""
+        return cell
+    }
+
+}
+
+
+// MARK: - UICollectionViewDelegate, UICollectionViewDelegateFlowLayout
+
+extension WeatherViewController: UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        // cellサイズ
+        let width = collectionView.bounds.width / CGFloat(collectionView.numberOfItems(inSection: 0))
+        return CGSize(width: width, height: 70)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        // cell間隔0
+        return 0
+    }
+    
 }
